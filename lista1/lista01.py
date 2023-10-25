a1a0379b624e42a83bcec18e6443fd8423c20db5
Michał Frąckowiak <69917212+MichalF-lab@users.noreply.github.com>
2023-10-25 17:30:48 +0200

    commit to master

--- lista1/lista01.py
+++ lista1/lista01.py
@@ -1,56 +1,13 @@
 import random
 import numpy
+import time
 
 # Przygotowanie tablic do testów
 
-main_table = numpy.arange(random.randint(5,7))
+# main_table = numpy.arange(random.randint(5,7))
 
-for item in main_table:
-    main_table[item] = float(random.randint(1,100))
-
-# # Funkcja pomocnicza
-# def swap(table, a, b):
-#    print(table," ",a," ",b)
-#    temp = table[a]
-#    table[a] = table[b]
-#    table[b] = temp
-#    print(table," ",a," ",b)
-
-# # def insertionsort(table):
-# #   for item in range(len(table)):
-# #      for Lower_temp in range(item,-1,-1):
-# #         # nie porównujemy tych samych elementów
-# #         if(item == Lower_temp): continue
-# #         print(Lower_temp, " ", item)  
-# #         if(table[item] < table[Lower_temp]):
-# #            swap(table,item, Lower_temp)
-# #            for this_loop in range(Lower_temp,-1,-1):
-# #               if(table[Lower_temp] < table[this_loop]):
-# #                 swap(table,this_loop, Lower_temp)
-# #   return table
-
-# # Funkcja pomocnicza
-# def swap_plus(table, a, b, insertionsortpluscount):
-#    temp = table[a]
-#    table[a] = table[b]
-#    table[b] = temp
-#    # print(table)
-#    insertionsortpluscount += 2
-#    return insertionsortpluscount
-
-# # def insertionsortplus(table):
-# #   insertionsortpluscount = 0
-# #   for item in range(len(table)):
-# #      for Lower_temp in range(item,-1,-1):
-# #         # nie porównujemy tych samych elementów
-# #         if(item == Lower_temp): continue
-# #         if(table[item] < table[Lower_temp]):
-# #            insertionsortpluscount = swap_plus(table,item, Lower_temp, insertionsortpluscount)
-# #            for this_loop in range(Lower_temp,-1,-1):
-# #               print(this_loop, table[Lower_temp] , table[this_loop])
-# #               if(table[Lower_temp] < table[this_loop]):
-# #                 insertionsortpluscount = swap_plus(table,this_loop, Lower_temp, insertionsortpluscount)
-# #   return insertionsortpluscount
+# for item in main_table:
+#     main_table[item] = float(random.randint(1,100))
 
 #-----------------------------------------------------------------------------
 
@@ -152,13 +109,179 @@ def mergesort(table):
    return sort(mergesort(table[:middle]), mergesort(table[middle:]))
       
 
-print(main_table)
-print(mergesort(main_table))
+
+def mergesortplus(table):
+   count_e, count_p = 0,0
+   def sort(table_a, table_b):
+      nonlocal count_e, count_p
+      #print(table_a," ",table_b)
+      index_a, index_b = 0,0
+      new_table = numpy.arange(len(table_a) + len(table_b))
+      for index in new_table:
+         if(index_a == len(table_a)):
+            while (index_b < len(table_b)):
+               new_table[index] = table_b[index_b]
+               index_b += 1
+               index += 1
+               count_e += 1
+            #print(new_table)
+            return new_table
+         
+         elif(index_b == len(table_b)):
+            while (index_a < len(table_a)):
+               new_table[index] = table_a[index_a]
+               index_a += 1
+               index += 1
+               count_e += 1
+            #print(new_table)
+            return new_table 
+         
+         count_p += 1
+         if(table_a[index_a] < table_b[index_b]):
+            new_table[index] = table_a[index_a]
+            index_a += 1
+            count_e += 1
+         else:
+            new_table[index] = table_b[index_b]
+            count_e += 1
+            index_b += 1
+      return new_table
+   
+   if(len(table) == 1): return table
+   if(len(table) == 2):
+      count_p += 1
+      if(table[1] < table[0]):
+         table[0], table[1] = table[1], table[0]
+         count_e += 2
+      return table
+   middle = len(table) // 2
+   sort(mergesort(table[:middle]), mergesort(table[middle:]))
+   return (count_p, count_e)
 
 
-#print(main_table)
-#print(main_table)
-# for item in range(len(main_table)):
-#     print (item)
+# print(main_table)
+# print(mergesortplus(main_table))
 
-# merge sort - n log n
+ #--------------------------------------------------------------------------
+
+def mergesort21(table):
+   def sort(table_a, table_b):
+      #print(table_a," ",table_b)
+      index_a, index_b = 0,0
+      new_table = numpy.arange(len(table_a) + len(table_b))
+      for index in new_table:
+         if(index_a == len(table_a)):
+            while (index_b < len(table_b)):
+               new_table[index] = table_b[index_b]
+               index_b += 1
+               index += 1
+            #print(new_table)
+            return new_table
+         
+         elif(index_b == len(table_b)):
+            while (index_a < len(table_a)):
+               new_table[index] = table_a[index_a]
+               index_a += 1
+               index += 1
+            #print(new_table)
+            return new_table 
+          
+         if(table_a[index_a] < table_b[index_b]):
+            new_table[index] = table_a[index_a]
+            index_a += 1
+         else:
+            new_table[index] = table_b[index_b]
+            index_b += 1
+      return new_table
+   
+   if(len(table) == 1): return table
+   if(len(table) == 2):
+      if(table[1] < table[0]):
+         table[0], table[1] = table[1], table[0]
+      return table
+   middle = len(table) // 3
+   return sort(mergesort(table[:middle]), mergesort(table[middle:]))
+      
+
+
+def mergesort21plus(table):
+   count_e, count_p = 0,0
+   def sort(table_a, table_b):
+      nonlocal count_e, count_p
+      #print(table_a," ",table_b)
+      index_a, index_b = 0,0
+      new_table = numpy.arange(len(table_a) + len(table_b))
+      for index in new_table:
+         if(index_a == len(table_a)):
+            while (index_b < len(table_b)):
+               new_table[index] = table_b[index_b]
+               index_b += 1
+               index += 1
+               count_e += 1
+            #print(new_table)
+            return new_table
+         
+         elif(index_b == len(table_b)):
+            while (index_a < len(table_a)):
+               new_table[index] = table_a[index_a]
+               index_a += 1
+               index += 1
+               count_e += 1
+            #print(new_table)
+            return new_table 
+         
+         count_p += 1
+         if(table_a[index_a] < table_b[index_b]):
+            new_table[index] = table_a[index_a]
+            index_a += 1
+            count_e += 1
+         else:
+            new_table[index] = table_b[index_b]
+            count_e += 1
+            index_b += 1
+      return new_table
+   
+   if(len(table) == 1): return table
+   if(len(table) == 2):
+      count_p += 1
+      if(table[1] < table[0]):
+         table[0], table[1] = table[1], table[0]
+         count_e += 2
+      return table
+   middle = len(table) // 2
+   sort(mergesort(table[:middle]), mergesort(table[middle:]))
+   return (count_p, count_e)
+
+
+# print(main_table)
+# print(mergesort21plus(main_table))
+
+ #--------------------------------------------------------------------------
+def tests(size = 20):
+      
+   main_table = numpy.arange(size)
+   for item in main_table:
+      main_table[item] = float(random.randint(1,400))
+   
+   copy1, copy2, copy3 = numpy.copy(main_table), numpy.copy(main_table), numpy.copy(main_table)
+   results = []
+   start = time.time()
+   results.append([size, insertionsortplus(copy1), "insertionsort"])
+   end = time.time()
+   results[0].append(end-start)
+   start = time.time()
+   results.append([size, bubblesortplus(copy2), "bubblesort"])
+   end = time.time()
+   results[1].append(end-start)
+   start = time.time()
+   results.append([size, mergesortplus(copy3), "mergesort"])
+   end = time.time()
+   results[2].append(end-start)
+   start = time.time()
+   results.append([size, mergesort21plus(main_table), "mergesort21"])
+   end = time.time()
+   results[3].append(end-start)
+
+   return results
+
+print(tests(6000))
